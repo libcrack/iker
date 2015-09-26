344e354369c40ce7a7e79ec4f16a5fed0ce464a9
Null Device <devnull@libcrack.so>
2015-09-26 13:09:30 +0200

    chg: TODO.md - Added 4 new bullet points to 2do list

--- TODO.md
+++ TODO.md
@@ -1,3 +1,8 @@
-# iker todo list
+# iker 2do list
 
+* Rewrite XML file output generation using sax or ElementTree
 * Rewrite using scapy IPSec support
+* Modularize all functions of iker/iker.py properly
+* Rewrite issues discover algorithm
+* Move issues data to external file (sqlite? CSV?)
+
--- iker/color.py
+++ iker/color.py
@@ -8,14 +8,18 @@ yellow = '\033[33m'
 blue = '\033[34m'
 reset = '\033[0m'
 
+
 def error(msg):
     print("[%sERROR%s] %s" % (red, reset, msg))
+
 
 def warning(msg):
     print("[%sWARNING%s] %s" % (yellow, reset, msg))
 
+
 def info(msg):
     print("[%sINFO%s] %s" % (green, reset, msg))
+
 
 def debug(msg):
     print("[%sDEBUG%s] %s" % (blue, reset, msg))
