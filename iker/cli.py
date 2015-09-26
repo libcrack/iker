5b54b26b865ad6ca301c5bba766bc12f9ef64c2f
Borja <borja@libcrack.so>
2015-09-26 13:08:08 +0200

    new: CLI added: cli.py loads iker.py:main()

--- iker/cli.py
+++ iker/cli.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import iker
 
-def main():
-    print("iker.main()")
+from . import iker
+import sys
+
+if __name__ == "__main__":
+    sys.exit(iker.main(sys.argv[1:]))
+
