833f1f4cadbce66bfdf35013f3993be697d6abfe
Borja R <borja@libcrack.so>
2015-12-08 21:49:33 +0100

    Added version variable to virtualenv.sh script

--- virtualenv.sh
+++ virtualenv.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 venv="env"
+v=3.5
 [[ "$BASH_SOURCE" == "$0" ]] && {
     myself="$(readlink -m ${0#-*})"
     echo "Usage: . $myself" > /dev/stderr
     exit 1
 } || {
-    [[ -d "$venv" ]] || virtualenv -p /usr/bin/python3.4 "$venv"
+    [[ -d "$venv" ]] || virtualenv -p "/usr/bin/python${v}" "$venv"
     echo ">> Entering virtual environment \"$venv\"" > /dev/stdout
     source "$venv/bin/activate"
 }
